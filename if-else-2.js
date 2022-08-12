d8bb040800e9a3bab5c21230f656c6d61f8dc3e3
wt8668 <110592829+wt8668@users.noreply.github.com>
2022-08-12 14:40:46 +0700

    Add files via upload

---
+++ if-else-2.js
@@ -0,0 +1,6 @@
+let value = prompt('What is the best game series of all time?',);
+if (value == 'Dark Souls') {
+    alert('Youre right');
+} else {
+    alert ("You don't know? It's still Dark Souls!");
+}
