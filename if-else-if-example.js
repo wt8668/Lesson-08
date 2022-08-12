2da0d9085bc9f25307f9a46f79a18284b99d6cec
wt8668 <110592829+wt8668@users.noreply.github.com>
2022-08-12 15:25:11 +0700

    Add files via upload

---
+++ if-else-if-example.js
@@ -0,0 +1,17 @@
+let userName = prompt("Who's there?");
+if (userName == 'Its Rick') {
+
+} else if (userName == null) {
+    alert('Youre not Rick!');
+} else {
+    alert ("I don't know you!");
+}
+    let pass = prompt('Password?');
+    if (pass = 'login') {
+        alert('Welcome, Rick!');
+    } else if (pass == null) {
+        alert('You aint no Rick!');
+    } else {
+        alert('Wrong');
+    }
+
